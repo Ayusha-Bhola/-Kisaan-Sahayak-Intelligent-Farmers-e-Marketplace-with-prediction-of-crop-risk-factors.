983c7e0dc31c05f983cd7c8257ec97c6041f11ba
Ayusha-Bhola <54286037+Ayusha-Bhola@users.noreply.github.com>
2021-07-01 15:40:57 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$conn = mysqli_connect("localhost", "root", "", "shopping");
-    $img = mysqli_query($conn, "SELECT * FROM product");
-     //while ($row = mysqli_fetch_array($img)) {     
-		
-      //	echo "<img src='Images/".$row['product_image']."' >";   
-      
-    //}     
-?>
